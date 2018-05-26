c9dc7142abefbc9c92132b38bd94c1be241284ff
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-05-25 21:24:06 -0500

    Added code to controller files. Changed 'routes' to 'controller' in server.js

---
+++ controller/events-api-routes.js
@@ -0,0 +1,23 @@
+var db = require("../models")
+
+module.exports = function(app){
+    db.get("/api/events", function(req, res){
+
+    });
+
+    db.get("/api/events/:id", function(req, res){
+
+    });
+
+    db.post("/api/events", function(req, res){
+
+    });
+
+    app.put("/api/events", function(req, res){
+
+    });
+
+    app.delete("/api/events/:id", function(req, res){
+
+    });
+}
