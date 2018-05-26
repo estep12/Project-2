95d08d7a47e3df5977154da6ee799a1d01cbc178
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-05-25 21:32:16 -0500

    Added more code to people and groups api routes

---
+++ controller/groups-api-routes.js
@@ -0,0 +1,23 @@
+var db = require("../models")
+
+module.exports = function(app){
+    db.get("/api/groups", function(req, res){
+
+    });
+
+    db.get("/api/groups/:id", function(req, res){
+
+    });
+
+    db.post("/api/groups", function(req, res){
+
+    });
+
+    app.put("/api/groups", function(req, res){
+
+    });
+
+    app.delete("/api/groups/:id", function(req, res){
+
+    });
+}
