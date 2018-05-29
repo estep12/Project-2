fb2d31aa57946dbb82bc5085192b862c96b3abd4
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-05-29 18:00:24 -0500

    Removed peoplegroups model. Added code to people-api-routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser")
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// var db = require("../models");
+var db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -23,10 +23,10 @@ app.set("view engine", "handlebars");
 require("./controller/html-routes.js")(app);
 require("./controller/events-api-routes.js")(app);
 require("./controller/groups-api-routes.js")(app);
-// require("./controller/people-api-routes.js")(app);
+require("./controller/people-api-routes.js")(app);
 
-// db.sequelize.sync({ force: true }). then(function() {
+db.sequelize.sync({ force: true }). then(function() {
     app.listen(PORT, function() {
         console.log("listening on PORT" + PORT);
     })
-// });
+});
