71035feed949f9467629fed0eaefbcd8655ce330
Becky Afzali <beckyafzali@gmail.com>
2018-06-01 15:53:02 -0500

    initial set up of user authentication and signup form modification

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express")
 var bodyParser = require("body-parser")
+const passport = require('passport');
+const session = require('express-session');
+const env = require('dotenv').load();
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -8,8 +11,8 @@ var db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());;
-app.use(bodyParser.text())
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 app.use(express.static("public"));
 
@@ -19,6 +22,16 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// For Passport
+app.use(session({ secret: 'lineup', resave: true, saveUninitialized: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistant login sessions
+
+// Routes
+const authRoute = require('./controller/user-auth-routes.js')(app, passport);
+
+// load passport strategies
+require('./config/passport/passport.js')(passport, db.user);
 
 require("./controller/html-routes.js")(app);
 require("./controller/events-api-routes.js")(app);
