f582b1d0326939286bb55be84658f910510022aa
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-06-03 17:52:58 -0500

    Update html-routes.js

--- controller/html-routes.js
+++ controller/html-routes.js
@@ -127,34 +127,34 @@ module.exports = function (app) {
 
 // =====================================================================
 
-module.exports = function(app){
+// module.exports = function(app){
 
-    app.get("/", function(req, res) {
-        console.log(req.user);
-        console.log(req.isAuthenticated());
-        res.render("index")
-      });
+//     app.get("/", function(req, res) {
+//         console.log(req.user);
+//         console.log(req.isAuthenticated());
+//         res.render("index")
+//       });
 
     //   app.get("/allEvents", function(req, res){
     //       res.render("events")
     //   });
 
-      app.get("/createEvent", function(req, res){
-        res.render("createevent")
-    });
+//       app.get("/createEvent", function(req, res){
+//         res.render("createevent")
+//     });
 
 
-    app.get("/createGroup", function(req, res){
+//     app.get("/createGroup", function(req, res){
 
-        res.render("creategroup")
-    });
+//         res.render("creategroup")
+//     });
 
-      app.get("/login", function(req, res){
-          res.render("login")
-      });
+//       app.get("/login", function(req, res){
+//           res.render("login")
+//       });
 
-      app.get("/signup", function(req, res){
-          res.render("signup")
-      });
-}
+//       app.get("/signup", function(req, res){
+//           res.render("signup")
+//       });
+// }
 
