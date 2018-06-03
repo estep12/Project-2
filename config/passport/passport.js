18812232357e6328891193bb8416a200fc2636d3
Becky Afzali <beckyafzali@gmail.com>
2018-06-03 13:01:32 -0500

    userAuth not working switching to clean branch

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -1,90 +1,148 @@
+const db = require("../../models");
 // load bcrypt
 const bCrypt = require('bcrypt-nodejs');
-const User = require('../../models/people.js');
+const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-module.exports = function(passport) {
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
+passport.serializeUser(function (user, done) {
+  console.log('serialize');
+  done(null, user.id);
+});
+
+// deserialize user
+passport.deserializeUser(function (id, done) {
+  db.People.findById(id, function (err, user) {
+    console.log(`deserialize ${user.userName}`);
+    done(err, user);
   });
+});
 
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'userNameLogin',
+    passwordField: 'passwordLogin',
+    passReqToCallback: true, // allows us to pass back the entire request to the callback
+  },
+  function (userName, password, done) {
+    console.log(userName);
+    console.log(password);
+    const isValidPassword = function (userpass, pass) {
+      return bCrypt.compareSync(pass, userpass);
+    };
 
-  // used to deserialize the user
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
+    db.People.findOne({ where: { userName: userName } }).then(function (user) {
+      if (!user) {
+        return done(null, false, { message: 'userName does not exist' });
+      }
+      if (!isValidPassword(user.password, password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
     });
-  });
+  },
+));
 
-  // local signup
-  passport.use('local-signup', new LocalStrategy(
-    {
-      usernameField: 'userName',
-      passwordField: 'password',
-      passReqToCallback: true, // allows us to pass back the entire request to the callback
-    },
+module.exports = passport;
 
-    function(req, userName, password, done) {
-      let generateHash = function(password) {
-        return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-      };
+// module.exports = function (passport) {
+  // passport.serializeUser(function (user, done) {
+  //   console.log('serialize');
+  //   done(null, user.id);
+  // });
 
-      User.findOne({ where: { userName: userName } }).then(function(user) {
-        if (user) {
-          return done(null, false, { message: 'That username is already taken' });
-        } else {
-          let userPassword = generateHash(password);
-          var data = { 
-            email: email,
-            password: userPassword,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName
-          };
+  // // deserialize user
+  // passport.deserializeUser(function (id, done) {
+  //   db.People.findById(id, function (err, user) {
+  //     console.log(`deserialize ${user.userName}`);
+  //     done(err, user);
+  //   });
+  // });
 
-          User.create(data).then(function(newUser, created) {
-            if (!newUser) {
-              return done(null,false);
-            }
-            if (newUser) {
-              return done(null, newUser);
-            }
-          });
-        }
-      });
-    },
-  ));
+  // // LOCAL SIGNIN ==================================================================================
+  // passport.use('login', new LocalStrategy(
+    // {
+    //   usernameField: 'userNameLogin',
+    //   passwordField: 'passwordLogin',
+    //   passReqToCallback: true, // allows us to pass back the entire request to the callback
+    // },
 
-  // LOCAL SIGNIN
-  passport.use('local-signin', new LocalStrategy(
-    {
-      // by default, local strategy uses username and password, we will override with email
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true, // allows us to pass back the entire request to the callback
-    },
+  //   function (userName, password, done) {
+  //     const isValidPassword = function (userpass, pass) {
+  //       return bCrypt.compareSync(pass, userpass);
+  //     };
 
-    function(req, email, password, done) {
-      let User = user;
-      let isValidPassword = function(userpass, password) {
-        return bCrypt.compareSync(password, userpass);
-      };
+  //     db.People.findOne({ where: { userName: userName } }).then(function (user) {
+  //       if (!user) {
+  //         return done(null, false, { message: 'userName does not exist' });
+  //       }
+  //       if (!isValidPassword(user.password, password)) {
+  //         return done(null, false, { message: 'Incorrect password.' });
+  //       }
 
-      User.findOne({ where: { email: email } }).then(function(user) {
-        if (!user) {
-          return done(null, false, { message: 'Email does not exist' });
-        }
-        if (!isValidPassword(user.password, password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
+  //       const userinfo = user.get();
 
-        let userinfo = user.get();
+  //       return done(null, userinfo);
+  //     }).catch(function (err) {
+  //       console.log('Error:', err);
 
-        return done(null, userinfo);
-      }).catch(function(err) {
-        console.log('Error:', err);
+  //       return done(null, false, { message: 'Something went wrong with your Signin' });
+  //     });
+  //   },
+  // ));
 
-        return done(null, false, { message: 'Something went wrong with your Signin' });
-      });
-    },
-  ));
-};
+//   // local signup ==================================================================================
+//   passport.use('signup', new LocalStrategy(
+//     {
+//       usernameField: 'userName',
+//       passwordField: 'password',
+//       passReqToCallback: true,
+//     },
+
+//     function (req, userName, password, done) {
+//       console.log('Made it into passport!!');
+//       // console.log(req.body);
+//       // serialize user
+//       console.log(done);
+
+//       db.People.findOne({ where: { userName: userName } }).then(function (user) {
+//         if (user) {
+//           console.log('username exists');
+//           return done(null, false, { message: 'Username already exists.' });
+//         }
+//         if (!user) {
+//           // create user with POST to db
+//           console.log("username doesn't exist");
+//           db.People.create(
+//             req.body,
+//             {
+//               include: [{
+//                 model: db.Group,
+//                 through: { attributes: [] },
+//               }],
+//             },
+//           ).then(function (dbPeople) {
+//             // res.json(dbPeople);
+//             // console.log("success", dbPeople);
+//             if (!dbPeople) {
+//               return done(null, false);
+//               console.log("NO dbPeople to return!!");
+//             }
+//             if (dbPeople) {
+//               console.log(dbPeople.userName);
+//               // db.People.findOne({ where: { userName: dbPeople.userName } }).then(function(user) {
+//               //   if (!user) throw err;
+//               //   console.log(user);
+//               //   req.login(user, function(request, res) {
+//               //     res.redirect('index');
+//               //   });
+//               // });
+//               return done(null, dbPeople);
+//             }
+//             // return dbPeople;
+//           });
+//         }
+//       });
+//     },
+//   ));
+// };
+
--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -1,26 +1,44 @@
-$(function() {
-    $("#sign-up").on("click", function(event){
-        event.preventDefault();
+$(function () {
+  $("#sign-up").on("click", function (event) {
+    event.preventDefault();
 
-        var newSignup = {
-            firstName: $("#firstName").val().trim(),
-            lastName: $("#lastName").val().trim(),
-            userName: $("#userName").val().trim(),
-            password: $("#password").val().trim(),
-            email: $("#email").val().trim(),
-            phoneNumber: $("#phoneNumber").val().trim(),
-        };
+    let newSignup = {
+      firstName: $("#firstName").val().trim(),
+      lastName: $("#lastName").val().trim(),
+      userName: $("#userName").val().trim(),
+      password: $("#password").val().trim(),
+      email: $("#email").val().trim(),
+      phoneNumber: $("#phoneNumber").val().trim(),
+    };
 
-        $.ajax("/api/people", {
-            type: "POST",
-            data: newSignup
-        }).then(function(){
-            console.log("Created New User");
-            // $.ajax("/api/people/:id", {
-            //     type: "GET",
+    $.ajax("/api/people", {
+      type: "POST",
+      data: newSignup,
+    });
 
-            // })
-            location.reload(); 
-        });
-    })
-})  
+    // $.ajax("/signup", {
+    //   type: "POST",
+    //   data: newSignup,
+    // }).then(function () {
+    //   // why does the following console log only show up if a username is submitted that already exists?? Page is refreshed before getting the AJAX response...
+    //   console.log("Created New User");
+    //   // location.reload();
+    // });
+  });
+});
+
+$(function () {
+  $("#login").on("click", function (event) {
+    event.preventDefault();
+
+    let newLogin = {
+      userName: $("#userNameLogin").val().trim(),
+      password: $("#passwordLogin").val().trim(),
+    };
+
+    $.ajax("/login", {
+      type: "POST",
+      data: newLogin,
+    });
+  });
+});
